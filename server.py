d2a401feaf81592a0eb378fc4d2ca92119ed666c
Ivan Filimonov <snake73w@gmail.com>
2017-01-21 17:53:34 +0700

    update server

--- server.py
+++ server.py
@@ -1,10 +1,39 @@
+from parse_afisha import get_movies_info
+
+import json
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.contrib.cache import SimpleCache
 
 app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+cache = SimpleCache()
+
 
 @app.route('/')
 def films_list():
-    return render_template('films_list.html')
+    movies = cache.get('movies')
+    if movies is None:
+        movies = get_movies_info()
+        cache.set('movies', movies, timeout=12 * 60 * 60)
+    return render_template('films_list.html', movies=movies)
+
+
+@app.route('/api/docs')
+def api_docs():
+    return render_template('api_docs.html')
+
+
+@app.route('/api/movies')
+def get_movies():
+    movies = cache.get('movies')
+    if movies is None:
+        movies = get_movies_info()
+        cache.set('movies', movies, timeout=12 * 60 * 60)
+    return json.dumps(movies, ensure_ascii=False)
+
 
 if __name__ == "__main__":
     app.run()
