ba3ee79ec428a1822ad91f664e6167dc05cb94e1
Ivan Filimonov <snake73w@gmail.com>
2017-02-14 10:07:32 +0700

    use .get for get rating from dict

--- parse_afisha.py
+++ parse_afisha.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 import re
 import json
 
@@ -14,7 +13,7 @@ def get_movies_urls(movies_count=10):
     return ['http:{}'.format(title.find('a')['href']) for title in titles][:movies_count]
 
 
-def fetch_movie_info(movie_url='https://www.afisha.ru/movie/212929/'):
+def fetch_movie_info(movie_url):
     movie_html = requests.get(movie_url).text
     soup = BeautifulSoup(movie_html, 'html.parser')
     data_from_script = soup.select('script[type="application/ld+json"]')[0].text
@@ -24,12 +23,11 @@ def fetch_movie_info(movie_url='https://www.afisha.ru/movie/212929/'):
     content = data_json['text']
     genre = data_json['genre']
     image = data_json['image']
-    rating = float(data_json['aggregateRating']['ratingValue'])
-    votes = data_json['aggregateRating']['ratingCount']
+    rating = float(data_json.get('aggregateRating', {}).get('ratingValue', 0))
+    votes = data_json.get('aggregateRating', {}).get('ratingCount', 0)
     return {'title': title, 'description': description, 'content': content, 'genre': genre,
             'image': image, 'rating': rating, 'votes': votes, 'url': movie_url}
 
 
 def get_movies_info():
     return [fetch_movie_info(url) for url in get_movies_urls()]
-
