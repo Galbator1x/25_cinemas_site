55b155b79104a7d752df4b1f013e24b5660b24fd
Ivan Filimonov <snake73w@gmail.com>
2017-02-14 09:58:58 +0700

    split config to dev and prod

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
-DEBUG = False
+class Config(object):
+    DEBUG = False
+    CSRF_ENABLED = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
